fda2ece17dcc01103c27ac6e37e1a8febf80f5ac
epsnaught <the.epsilon.naught@gmail.com>
2022-02-05 11:10:18 -0500

    Add simple auth provider (temporary)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { App } from './App';
 
 import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<p>Hello World</p>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
